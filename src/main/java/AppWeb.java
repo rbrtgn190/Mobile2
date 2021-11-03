fad719b739a8d32c0508dec1bc20f529f741aaae
rbrtgn190 <rbrtgn190@gmail.com>
2021-11-03 18:52:06 +0100

    Added UFTM local / SaaS and Appium Local variations

--- src/main/java/AppWeb.java
+++ src/main/java/AppWeb.java
@@ -29,6 +29,36 @@ public class AppWeb  {
     private WebElement userBtn;
     private WebElement createNewAccountBtn;
 
+    // UFTM SaaS
+    public static final String UFTM_SERVER = "https://demo.mobilecenter.io";
+    public static final String UFTM_SERVER_USERNAME = "jeanmatthieu.guerin@microfocus.com";
+    public static final String UFTM_SERVER_PASSWORD = "Welc0me11";
+    public static final String UFTM_UDID = "ce08171898ee74520c7e";
+
+    // UFTM NimbusServer
+/*    public static final String UFTM_SERVER = "http://nimbusserver.aos.com:8084";
+    public static final String UFTM_SERVER_USERNAME = "admin@default.com";
+    public static final String UFTM_SERVER_PASSWORD = "Password1";
+    // "client=oauth2-WAFwAbtcj99aKpHNQIVH@microfocus.com; secret=8beMYgc4OHhTrlW8KQ0Y; tenant=999999999; url=nimbusserver.aos.com:8084;"
+    public static final String UFTM_UDID = "";*/
+
+    // Appium local
+/*    public static final String UFTM_SERVER = "http://127.0.0.1:4723";
+    public static final String UFTM_SERVER_USERNAME = "";
+    public static final String UFTM_SERVER_PASSWORD = "";
+    public static final String UFTM_UDID = "";*/
+
+
+    //Device OS needs to be set to android or iOS
+    public static final String DEVICE_OS = "Android";
+
+/*    //The Following is only required if Anonymous Access disabled
+    public static final String UFTM_SERVER_CLIENT_ID = "oauth2-…";
+    public static final String UFTM_SERVER_CLIENT_SECRET = "ke…";
+    public static final String UFTM_SERVER_TENANT_ID = "999999999";
+    public static final String UFTM_SERVER_WORKSPACE_NAME = "Default workspace";*/
+
+
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
         // Created object of DesiredCapabilities class.
@@ -36,13 +66,19 @@ public class AppWeb  {
 
         //cap.setCapability("deviceName","Nexus");
         cap.setCapability("platformName", "Android");
+        cap.setCapability("udid", UFTM_UDID);
         //cap.setCapability("automationName", "UiAutomator1");
         //cap.setCapability("appPackage", "com.scores365");
         //cap.setCapability("appActivity", "com.scores365.ui.Splash");
         cap.setCapability("browserName", "chrome");
+        //cap.setCapability("chromedriverUseSystemExecutable", "true");
+
+        cap.setCapability("userName", UFTM_SERVER_USERNAME);
+        cap.setCapability("password", UFTM_SERVER_PASSWORD);
 
 
-        driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+        //driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+        driver = new RemoteWebDriver(new URL(UFTM_SERVER + "/wd/hub"), cap);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 10);
     }
