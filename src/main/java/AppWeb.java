7ccdd10460f71a0b4ed0b0146e53b0db4d37f2a4
rbrtgn190 <rbrtgn190@gmail.com>
2021-11-07 11:20:39 +0100

    Ø Branch TruAppium 	[0711-1120] 	[x] Make it a real appium test https://admhelp.microfocus.com/uftmobile/en/2021/Content/Appium/Appium_EG_java.htm  : import io.appium.java_client. [x] Retrieve appium logs : https://admhelp.microfocus.com/uftmobile/en/2021/Content/Appium/Appium_Setup.htm#mt-item-6

--- src/main/java/AppWeb.java
+++ src/main/java/AppWeb.java
@@ -9,6 +9,10 @@ import com.hpe.leanft.selenium.ByEach;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.ios.IOSDriver;
+
 import java.net.URL;
 import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
@@ -22,21 +26,23 @@ public class AppWeb  {
     //Change this constructor to private if you supply your own public constructor
     }
 
-    private static WebDriver driver;
+//    private static WebDriver driver;
     private static WebDriverWait wait;
+/*    private static AndroidDriver AWebDriver = null;  //Android Driver
+    private static IOSDriver iOSWebDriver = null;	//IOS Driver*/
+    private static AppiumDriver driver;
+
     //Declaring elements for tests
     //Elements for registration test
-    private WebElement userBtn;
-    private WebElement createNewAccountBtn;
+//    private WebElement userBtn;
+//    private WebElement createNewAccountBtn;
 
 
-/*
     public static final String PLATFORM_NAME = "Android";
     public static final String BROWSER_NAME = "chrome";
-*/
 
-    public static final String PLATFORM_NAME = "iOS";
-    public static final String BROWSER_NAME = "Safari";
+/*    public static final String PLATFORM_NAME = "iOS";
+    public static final String BROWSER_NAME = "Safari";*/
 
 
 /*    // UFTM SaaS
@@ -46,20 +52,18 @@ public class AppWeb  {
     public static final String UFTM_UDID = "ce08171898ee74520c7e";*/
 
     // UFTM CAAS
-    public static final String UFTM_SERVER = "https://uftm-caas.saas.microfocus.com";
+/*    public static final String UFTM_SERVER = "https://uftm-caas.saas.microfocus.com";
     public static final String UFTM_SERVER_USERNAME = "jeanmatthieu.guerin@microfocus.com";
     public static final String UFTM_SERVER_PASSWORD = "Welc0me";
     // "client=oauth2-WAFwAbtcj99aKpHNQIVH@microfocus.com; secret=8beMYgc4OHhTrlW8KQ0Y; tenant=999999999; url=nimbusserver.aos.com:8084;"
-    public static final String UFTM_UDID = "";
+    public static final String UFTM_UDID = "";*/
 
-/*
     // UFTM NimbusServer
     public static final String UFTM_SERVER = "http://nimbusserver.aos.com:8084";
     public static final String UFTM_SERVER_USERNAME = "admin@default.com";
     public static final String UFTM_SERVER_PASSWORD = "Password1";
     // "client=oauth2-WAFwAbtcj99aKpHNQIVH@microfocus.com; secret=8beMYgc4OHhTrlW8KQ0Y; tenant=999999999; url=nimbusserver.aos.com:8084;"
     public static final String UFTM_UDID = "";
-*/
 
     // Appium local
 /*    public static final String UFTM_SERVER = "http://127.0.0.1:4723";
@@ -97,14 +101,71 @@ public class AppWeb  {
 
 
         //driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+/*
         driver = new RemoteWebDriver(new URL(UFTM_SERVER + "/wd/hub"), cap);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 10);
+*/
+        if(DEVICE_OS.equalsIgnoreCase("android"))
+        {
+            // Set Device
+/*            caps.setCapability("platformName", "Android");
+            //caps.setCapability("deviceName", "Pixel 2");
+            caps.setCapability("automationName", "android");
+
+            // App Capabilities
+            caps.setCapability("appPackage", "com.Advantage.aShopping");
+            caps.setCapability("appActivity", "com.Advantage.aShopping.SplashActivity");*/
+
+            // Create Web Driver
+            driver = new AndroidDriver(new URL(UFTM_SERVER + "/wd/hub"), cap);
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            wait = new WebDriverWait(driver, 10);
+
+            //Create a session to the UFTM server
+
+            System.out.println("UFTM session was successfully created [Android Device]");
+            //AWebDriver.quit();
+
+        }
+
+
+        if(DEVICE_OS.equalsIgnoreCase("iOS"))
+        {
+
+            // Set Device
+/*
+            caps.setCapability("platformName", "iOS");
+            caps.setCapability("deviceName", "iPhone 7");
+
+            // App Capabilities
+            caps.setCapability("bundleId", "com.hpe.iShopping");
+*/
+
+            // Create Web Driver
+            driver = new IOSDriver(new URL(UFTM_SERVER + "/wd/hub"), cap);
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            wait = new WebDriverWait(driver, 10);
+
+
+            System.out.println("UFTM session was successfully created [iOS Device]");
+            //iOSWebDriver.quit();
+
+        }
     }
 
     @AfterClass
     public static void tearDownAfterClass() throws Exception {
+        System.out.println("Quit driver");
         driver.quit();
+/*        if(DEVICE_OS.equalsIgnoreCase("android"))
+        {
+            AWebDriver.quit();
+        }
+        if(DEVICE_OS.equalsIgnoreCase("iOS"))
+        {
+            iOSWebDriver.quit();
+        }*/
     }
 
     @Before
@@ -115,6 +176,9 @@ public class AppWeb  {
 
     @After
     public void tearDown() throws Exception {
+
+
+
     }
 
     @Test
@@ -122,38 +186,17 @@ public class AppWeb  {
         driver.navigate().to("http://advantageonlineshopping.com");
         Thread.sleep(10*1000);
 
-
         driver.findElement(By.xpath("//div[@id='mobile-btn']")).click();
         Thread.sleep(1*1000);
         driver.findElement(By.xpath("//*[@id='mobileUser']")).click();
 
-
-        // /html/body/login-modal/div/div/div[3]/a[2]
-        //        <div class="displayed">Username</div>
-
-        ///*WebElement*/ createNewAccountBtn = By.xpath("/html/body/login-modal/div/div/div[3]/a[2]");
- /*       By byCreateNewAccountBtn = By.xpath("/html/body/login-modal/div/div/div[3]/a[2]");
-        createNewAccountBtn = wait.until(ExpectedConditions.presenceOfElementLocated(byCreateNewAccountBtn));
         Thread.sleep(5*1000);
-        Assert.assertTrue((ElementcreateNewAccountBtn.isDisplayed());*/
-        //Assert.assertTrue(driver.findElement(By.xpath("/html/body/login-modal/div/div/div[3]/a[2]")).isDisplayed());
 
- /*       // open users page - takes a while so wait until it is clickable
-        userBtn = wait.until(ExpectedConditions.elementToBeClickable(org.openqa.selenium.By.xpath("/html/body/header/nav/ul/li[3]/a/a")));
-        //userBtn = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("svg#menuUser")));
-        //driver.findElement(By.cssSelector("svg#menuUser"));
-// or this
-        userBtn = driver.findElement(By.xpath("//*[@id='menuUser']"));
-        //wait.until(ExpectedConditions.elementToBeClickable(userBtn));
-        userBtn.click();
-*/
 
-/*
-        // createNewAccountBtn = driver.findElement(By.className("create-new-account.ng-scope"));
-        // above fails. Selenium sees classNames with a space in them as a "compound selecter" and fails
-        createNewAccountBtn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/login-modal/div/div/div[3]/a[2]")));
-        Thread.sleep(1*1000); // even with the above, need to sleep a bit*/
-
-        Thread.sleep(5*1000);
+        // Retrieve Appium logs
+        HashMap<String, String> encoding= new HashMap<String, String>();
+        encoding.put("encoding", "UTF-8");
+        String logFileContents = (String)  driver.executeScript("mc-wd: downloadLogs", encoding);
+        System.out.println(logFileContents);
     }
 }
